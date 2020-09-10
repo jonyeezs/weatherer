946354a8b1f18c7fe0e0a4dd8388f7dd9b82b998
Jonathan Yee <jonyeezs@gmail.com>
2020-09-10 22:44:57 +1000

    chore(infrastructure): install tailwindcss
    Omg this took awhile. Every single tutorial had the old version of postcss-loader. The latest (since
    2 days ago) has MAJOR breaking changes:
    https://github.com/webpack-contrib/postcss-loader/blob/master/CHANGELOG.md#-breaking-changes

---
+++ webapp/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: "postcss-loader",
+        options: {
+          postcssOptions: {
+            syntax: "postcss-scss",
+            plugins: [
+              require.resolve("postcss-import"),
+              require.resolve("tailwindcss"),
+              require.resolve("autoprefixer"),
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
